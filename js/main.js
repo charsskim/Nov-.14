c77ff658dd602227b7a2d4b07860cbfe8a85ee6b
charsskim <mcprince73@naver.com>
2023-11-14 10:47:42 +0900

    split 함수에 선택자문자열이 아닌 DOM자체에 연결되도록 수정

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
+const main = document.querySelector('main');
+const btns = main.querySelectorAll('li');
+const boxs = main.querySelectorAll('article');
+const tits = main.querySelectorAll('h1');
+
+splitText(tits[0]);
+splitText(tits[1]);
+
+btns.forEach((btn, idx) => {
+	btn.addEventListener('click', () => {
+		activation(btns, idx);
+		activation(boxs, idx);
+	});
+});
+
+function activation(arr, idx) {
+	arr.forEach((el) => el.classList.remove('on'));
+	arr[idx].classList.add('on');
+}
 function splitText(el) {
-	const dom = document.querySelector(el);
 	let tags = '';
-	for (let letter of dom.innerText) {
+	for (let letter of el.innerText) {
 		tags += `<span>${letter}</span>`;
-		dom.innerHTML = tags;
+		el.innerHTML = tags;
 	}
 }
