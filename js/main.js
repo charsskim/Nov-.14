9a7e94b999d4343104191b1f88bb9b00e7407ab5
charsskim <mcprince73@naver.com>
2023-11-14 10:27:38 +0900

    탭매뉴 스타일링

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-/*const h1 = document.querySelector('h1');
-const txt = h1.innerText;
-let tags = '';*/
-
-//미션 위의 내용을 처리해주는 함수 제작(인수로 선택자 전달)
-
-splitText('h1');
-splitText('h2');
-
 function splitText(el) {
 	const dom = document.querySelector(el);
 	let tags = '';
