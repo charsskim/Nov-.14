e0c9baee86a3d852c10025d7ad395c0553f499d0
charsskim <mcprince73@naver.com>
2023-11-14 09:56:07 +0900

    for of 반복문으로 문자마다 span으로 분리해주느 기능 구현

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const h1 = document.querySelector('h1');
+const txt = h1.innerText;
+let tags = '';
+
+for (let letter of txt) {
+	tags += `<span>${letter}</span>`;
+}
+console.log(tags);
+h1.innerHTML = tags;
+
+//미션 위의 내용을 처리해주는 함수 제작(인수로 선택자 전달)
